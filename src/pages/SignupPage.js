e4e0363f71be26398bff03c6e5416f16802cc69e
Razi <12abdulrahim21@gmail.com>
2023-06-17 23:02:19 +0530

    login signup home pages templates set with routing

---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup } from '../features/auth/components/Signup'
+
+const SignupPage = () => {
+  return (
+    <div><Signup /></div>
+  )
+}
+
+export default SignupPage
