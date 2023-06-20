2235c98d46337c2799130cf113d3c7c45dff6366
Razi <12abdulrahim21@gmail.com>
2023-06-20 14:38:20 +0530

    cart ui  done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { Cart } from './features/cart/Cart';
+import CartPage from './pages/CartPage';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,10 @@ const router = createBrowserRouter([
   },{
     path: "/signup",
     element: <SignupPage></SignupPage>,
+  },
+  {
+    path: "/cart",
+    element: <CartPage></CartPage>,
   },
 ]);
 function App() {
